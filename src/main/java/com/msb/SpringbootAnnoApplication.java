b2b054030a59ad9ab04966f290036de3d74df6a2
xurb <xurongbiao@hmtim.com>
2022-05-28 16:19:12 +0800

    Configuration  注解的配置

--- src/main/java/com/msb/SpringbootAnnoApplication.java
+++ src/main/java/com/msb/SpringbootAnnoApplication.java
@@ -1,5 +1,6 @@
 package com.msb;
 
+import com.msb.config.MyConfig;
 import com.msb.pojo.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
@@ -17,8 +18,32 @@ public class SpringbootAnnoApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(SpringbootAnnoApplication.class, args);
         //从容器中取出bean，获取容器中的bean
-        User user01 = context.getBean("user01", User.class);
-        System.out.println(user01);
+//        User user01 = context.getBean("user01", User.class);
+//        User user02 = context.getBean("user2", User.class);
+//        System.out.println(user01);
+//        System.out.println(user02);
+//        System.out.println(user01==user02);
+        //容器都初始化了哪些对象
+//        String[] beanDefinitionNames = context.getBeanDefinitionNames();
+//        for (String name:beanDefinitionNames) {
+//            System.out.println(name);
+//        }
+
+        //获取MyConfig对象，单例模式
+        MyConfig myConfig1 = context.getBean(MyConfig.class);
+        MyConfig myConfig2 = context.getBean(MyConfig.class);
+        System.out.println(myConfig1 == myConfig2);
+
+        //new的方法获得的是两个内存对象
+        MyConfig myConfig3 = new MyConfig();
+        //容器获取的myConfig1，是从注解中获取的bean，代理bean方法，获取到的myConfig1，是一个代理bean对象
+        User user1 = myConfig1.getUser();
+        User user2 = myConfig1.getUser();
+        System.out.println(user1 == user2);
+        System.out.println(myConfig1 == myConfig3);
+
+
+
     }
 
 }
--- src/main/java/com/msb/config/MyConfig.java
+++ src/main/java/com/msb/config/MyConfig.java
@@ -4,13 +4,23 @@ import com.msb.pojo.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Configuration(proxyBeanMethods = true)
+/**
+ * proxyBeanMethods = false  like模式  启动快
+ *
+ * proxyBeanMethods = true  full模式  通过代理对象，依赖spring容器控制bean单例
+ */
 public class MyConfig {
 
     /*<bean id="" name="" class="com.msb.pojo.User"></bean>*/
     @Bean  //向容器中声明一个组件  User对象，方法名默认bean的id，返回值类型就是bean的class
     public User user01(){
         return new User(1,"zhangsan","123456");
+    }
+
+    @Bean("user02")
+    public User getUser(){
+        return new User(2,"lisi","123456");
     }
 
 }
